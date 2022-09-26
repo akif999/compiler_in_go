d00d5d9f800eff14645fe6171cb7831e535d10c4
akif999 <aki01nf28@gmail.com>
2022-09-26 17:06:02 +0900

    p43 (add compiler package)

---
+++ go/Monkey/compiler/compiler.go
@@ -0,0 +1,36 @@
+package compiler
+
+import (
+	"go/ast"
+
+	"github.com/akif999/compiler_in_go/go/Monkey/code"
+	"github.com/akif999/compiler_in_go/go/Monkey/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []ast.Object
+}
