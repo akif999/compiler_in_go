6d3782b9e3bd5b39a9bc0a1162ab8f7da347715c
akif999 <aki01nf28@gmail.com>
2022-11-24 15:45:59 +0900

    p182 adding Index operator

--- go/Monkey/code/code.go
+++ go/Monkey/code/code.go
@@ -39,6 +39,7 @@ const (
 
 	OpArray
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -67,6 +68,7 @@ var definitions = map[Opcode]*Definition{
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
