43a142d4e5bf8f89fc3cd0100d5e5784b682dc37
akif999 <aki01nf28@gmail.com>
2022-11-24 11:36:58 +0900

    p166 update compiler pkg (adding ArrayLiteral)

--- go/Monkey/code/code.go
+++ go/Monkey/code/code.go
@@ -36,6 +36,8 @@ const (
 
 	OpGetGlobal
 	OpSetGlobal
+
+	OpArray
 )
 
 type Definition struct {
@@ -62,6 +64,7 @@ var definitions = map[Opcode]*Definition{
 	OpNull:          {"OpNull", []int{}},
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
+	OpArray:         {"OpArray", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
