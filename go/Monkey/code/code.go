75a2adf34759aca687791d8e847368f5db651aa0
akif999 <aki01nf28@gmail.com>
2022-12-17 22:19:42 +0900

    P300 add definition of Closure to code pkg

--- go/Monkey/code/code.go
+++ go/Monkey/code/code.go
@@ -49,6 +49,8 @@ const (
 	OpReturn
 
 	OpGetBuiltin
+
+	OpClosure
 )
 
 type Definition struct {
@@ -84,6 +86,7 @@ var definitions = map[Opcode]*Definition{
 	OpReturnValue:   {"OpReturnValue", []int{}},
 	OpReturn:        {"OpReturn", []int{}},
 	OpGetBuiltin:    {"OpGetBuiltin", []int{1}},
+	OpClosure:       {"OpClosure", []int{2, 1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -158,6 +161,8 @@ func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
 		return def.Name
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	case 2:
+		return fmt.Sprintf("%s %d %d", def.Name, operands[0], operands[1])
 	}
 
 	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
